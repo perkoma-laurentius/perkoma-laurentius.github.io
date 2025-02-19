147ba5c8546f93875400f7619873339470a3e64c
christyuda <chrisyuda@ulbi.ac.id>
2025-02-19 23:50:26 +0700

    Update base URL for production and clean up auth-login.html structure

--- assets/js/config/baseUrl.js
+++ assets/js/config/baseUrl.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'https://backend-sikad.cloud'; // production
-export const BASE_URL = 'http://localhost:3003'; 
+export const BASE_URL = 'https://perkoma.backend-sikad.cloud'; // production
+// export const BASE_URL = 'http://localhost:3003'; 
